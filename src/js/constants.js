a9228f7202db9afee6b23125d417a6cc79715891
Antay <vandervise465@gmail.com>
2019-09-07 11:45:56 +0300

    Initial commit

---
+++ src/js/constants.js
@@ -0,0 +1,4 @@
+export const constants = {
+  ROWS: 20,
+  COLS: 50
+};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { constants } from "./js/constants";
+
+document.addEventListener('DOMContentLoaded', () => {
+  alert(`A little bit of es6 code ${constants.ROWS} ${constants.COLS}`);
+});
